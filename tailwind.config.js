38b12a7a12e6d72288c6d6d2f44001b45ebcccb6
Umesh Gayashan <138327662+UmeshGayashan@users.noreply.github.com>
2024-06-05 21:59:10 +0530

    ./app folder path added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
+  // content: ["./App.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}"], 
+  // any file with these file types in any folder which is in app folder
   theme: {
     extend: {},
   },
