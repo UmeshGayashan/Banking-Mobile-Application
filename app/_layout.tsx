f8ef4146b470351c19b0a8398fd6b140f9cf4508
Umesh Gayashan <138327662+UmeshGayashan@users.noreply.github.com>
2024-06-06 08:33:54 +0530

    Styling type has to change

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,19 @@
-import { View, Text } from 'react-native'
-import {Slot} from "expo-router";
-
-import React from 'react'
+import { View } from 'react-native';
+import { Slot } from 'expo-router';
+import React from 'react';
 
 const Layout = () => {
-  return <Slot/>;
+  return (
+    <View style={styles.container}>  
+      <Slot />
+    </View>
+  );
+};
+// In React Native, you should use the style prop instead of the className prop
+const styles = {
+  container: {
+    marginTop: 20, // equivalent to mt-5
+  },
 };
 
-export default Layout
+export default Layout;
